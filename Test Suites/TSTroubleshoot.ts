92c67342abb820e7310a9707bb7494ea0674873c
Wanda Hayes <nielsen.wh@hotmail.com>
2018-06-04 16:48:06 -0700

    Committing simple test for troubleshooting

---
+++ Test Suites/TSTroubleshoot.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Troubleshooting test suite</description>
+   <name>TSTroubleshoot</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9e4c5bd0-aa71-4195-8e2a-26688a22ac8a</testSuiteGuid>
+   <testCaseLink>
+      <guid>98653403-b398-4336-acf6-d8e970fc336a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TroubleshootMakeAppt</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
