c849be7b171becfa4646ffe512c4da22fef9a0f0
Wanda Hayes <nielsen.wh@hotmail.com>
2018-07-10 14:10:57 -0400

    WH Adding a TC that will fail in TSTroubleshootFail

---
+++ Test Suites/TSTroubleshootFail.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Troubleshooting test suite</description>
+   <name>TSTroubleshootFail</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>04e5b6e2-2b0f-407e-97d1-270e9ecc71b3</testSuiteGuid>
+   <testCaseLink>
+      <guid>98653403-b398-4336-acf6-d8e970fc336a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TroubleshootMakeAppt</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7660256a-0577-48f1-b95d-2086e729fcb3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TroubleshootMakeApptFail</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
