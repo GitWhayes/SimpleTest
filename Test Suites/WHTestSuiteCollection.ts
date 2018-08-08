46d627428484811c15d2272dc2bdf2f9196f39e8
Wanda Hayes <nielsen.wh@hotmail.com>
2018-08-08 13:05:19 -0400

    Pushing w/collection

---
+++ Test Suites/WHTestSuiteCollection.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>WHTestSuiteCollection</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e2cee92d-2eb6-4874-887d-1fe4be53a815</testSuiteGuid>
+   <testCaseLink>
+      <guid>d0cdda47-37cc-4146-a74b-2ac1d5b6eb5b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WHTroubleshootMakeAppt</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>efb79b76-17c6-47d1-bd46-6f46c03f602f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WHTroubleshootMakeApptFail</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
